d4bd3e5584e82bda706111e37cb25f3428f96147
Idan Masas <66463744+idanii@users.noreply.github.com>
2021-10-01 19:32:06 +0300

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -328,30 +328,30 @@ int FindClosestEnemy() {
 	return ClosestEntity;
 }
 
-char* getName(int index)
+char* getName(int index) // this returns player's name (and even more data)
 {
 	struct player_info
 	{
-		__int64         unknown;            //0x0000 
+		__int64 unknown;            //0x0000 
 		union
 		{
-			__int64       steamID64;          //0x0008 - SteamID64
+			__int64 teamID64;          //0x0008 - SteamID64
 			struct
 			{
-				__int32     xuid_low;
-				__int32     xuid_high;
+				__int32 xuid_low;
+				__int32 xuid_high;
 			};
 		};
-		char            szName[128];        //0x0010 - Player Name
-		int             userId;             //0x0090 - Unique Server Identifier
-		char            szSteamID[20];      //0x0094 - STEAM_X:Y:Z
-		char            pad_0x00A8[0x10];   //0x00A8
-		unsigned long   iSteamID;           //0x00B8 - SteamID 
-		char            szFriendsName[128];
-		bool            fakeplayer;
-		bool            ishltv;
-		unsigned int    customfiles[4];
-		unsigned char   filesdownloaded;
+		char szName[128];        //0x0010 - Player Name
+		int userId;             //0x0090 - Unique Server Identifier
+		char szSteamID[20];      //0x0094 - STEAM_X:Y:Z
+		char pad_0x00A8[0x10];   //0x00A8
+		unsigned long iSteamID;           //0x00B8 - SteamID 
+		char szFriendsName[128];
+		bool fakeplayer;
+		bool ishltv;
+		unsigned int customfiles[4];
+		unsigned char filesdownloaded;
 	} var;
 
 	DWORD clientState = MemClass.readMem<DWORD>(val.engineModule + offset.dwClientState);
